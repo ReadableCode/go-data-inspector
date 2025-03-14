ec5d5ab1c56fbb94e57994bb9fd89980623a4b3a
jason <jasonwchristiansen@gmail.com>
2025-03-14 00:57:01 -0500

    add csv file handling

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"flag"
 	"os"
@@ -10,11 +11,35 @@ func main() {
 	filePath := flag.String("file","","Path to the Input File")
 	flag.Parse()
 
+	// Check if the file path is provided
 	if *filePath == "" {
 		fmt.Println("Usage: goinspector --file <path to the input file>")
 		os.Exit(1)
 	}
 	
+	// Read the csv file with the csv reader function
+	data, err := readCSV(*filePath)
+	if err != nil {
+		fmt.Println("error reading CSV:", err)
+		os.Exit(1)
+	}
+	
+	fmt.Println("CSV Contents:")
+	for _, row := range data {
+		fmt.Println(row)
+	}
+	
 	fmt.Println("Go Data Inspector - CLI Tool")
 	fmt.Println("Processing file:", *filePath)
+}
+
+func readCSV(filePath string) ([][]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	
+	reader := csv.NewReader(file)
+	return reader.ReadAll() // reads the entire file into memory
 }

74f2e46505c6ec92dbeefc817478b05c0ca97027
jason <jasonwchristiansen@gmail.com>
2025-03-15 22:16:27 -0500

    add filtering and sorting, sort removes filter still though

--- backend/web.go
+++ backend/web.go
@@ -2,23 +2,41 @@ package main
 
 import (
 	"encoding/csv"
+	"fmt"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-var templatesDir = "../frontend/templates"
+var storedData [][]string // Store uploaded CSV data
 
 func hostSiteWithFiber() {
 	app := fiber.New()
 
-	// Serve frontend files
-	app.Static("/", "../frontend/templates/index.html")
+	// Serve static files (CSS, JS, etc.)
+	app.Static("/static", "../frontend/static")
 	app.Static("/css", "../frontend/css")
 
-	// Upload route
+	// Serve HTML template
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendFile("../frontend/templates/index.html")
+	})
+
+	// Upload CSV
 	app.Post("/upload", handleUpload)
 
-	app.Listen(":8505")
+	// Filter CSV
+	app.Get("/filter", handleFilter)
+
+	// Sort CSV
+	app.Get("/sort", handleSort)
+
+	// Start server
+	fmt.Println("Server running on http://localhost:8505")
+	err := app.Listen(":8505")
+	if err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
 }
 
 func handleUpload(c *fiber.Ctx) error {
@@ -34,10 +52,42 @@ func handleUpload(c *fiber.Ctx) error {
 	defer src.Close()
 
 	reader := csv.NewReader(src)
-	data, err := reader.ReadAll()
+	storedData, err = reader.ReadAll() // Store in memory
 	if err != nil {
 		return c.Status(500).SendString("Failed to parse CSV")
 	}
 
-	return c.JSON(fiber.Map{"data": data})
+	return c.JSON(fiber.Map{"data": storedData})
+}
+
+func handleFilter(c *fiber.Ctx) error {
+	column := c.Query("column")
+	condition := c.Query("condition")
+
+	if storedData == nil || len(storedData) < 2 {
+		return c.Status(400).SendString("No data uploaded yet")
+	}
+
+	filteredData, err := applyFilter(storedData, column+condition)
+	if err != nil {
+		return c.Status(400).SendString(err.Error())
+	}
+
+	return c.JSON(fiber.Map{"data": filteredData})
+}
+
+func handleSort(c *fiber.Ctx) error {
+	column := c.Query("column")
+	desc, _ := strconv.ParseBool(c.Query("desc"))
+
+	if storedData == nil || len(storedData) < 2 {
+		return c.Status(400).SendString("No data uploaded yet")
+	}
+
+	err := sortCSV(storedData, column, desc)
+	if err != nil {
+		return c.Status(400).SendString(err.Error())
+	}
+
+	return c.JSON(fiber.Map{"data": storedData})
 }
